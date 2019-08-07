891ea20d788785ea98aeae7ec170f5a3e3f63ce4
popyue <l20382006@gmail.com>
2019-08-07 12:05:54 +0800

    add comment

--- 591_scrapy/scrapy.py
+++ 591_scrapy/scrapy.py
@@ -30,33 +30,43 @@ with open("token.txt", 'r') as tokenfile:
 tokenfile.close()
 
 def main():
+	# Start Parse 591
 	global flag
+	# Site be parsed
 	url = "https://rent.591.com.tw/?kind=1&order=money&orderType=desc&region=1&section=5&rentprice=5&pattern=3"
 	session_requests = requests.session()
 	res = session_requests.get(url)
 	res.encoding = 'utf-8'
 
 	soup_Daan = BeautifulSoup(res.text,'html.parser')
-	#print(soup_Daan)
 	tag_Daan_csrfval = soup_Daan.find("meta" , {"name":"csrf-token"})['content']
 	tag_Daan_total = soup_Daan.find("div" , {"class":"pull-left hasData"})
 	totalitem = int(tag_Daan_total.find('i').string)
 	print(totalitem)
+
+	# limit item number in 30 
+	# If totalitem bigger than 30, page will change(Every page's item limit in 30)
+	# If the page change the data will reload
+	# This may cause the same item will apper again and again 
+	# And we will parse the duplicate data
 	if totalitem > 30:
 		totalitem = 30
 
 	# Get json Response
+	# this url is the query url that 591 site will send to backend and get a json response back 
 	url_search = "https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=1&order=money&orderType=desc&region=1&section=5&rentprice=5&pattern=3"
 	headers = {"X-Requested-With": "XMLHttpRequest","X-CSRF-TOKEN":tag_Daan_csrfval}
 	res2 = session_requests.get(url_search, headers=headers)
 	data = json.loads(res2.text)
-	#print(data.keys())
 	house_detail_all = data['data'] #get house data
 	for house in range(totalitem):
 		postID.append(house_detail_all.get('data')[house].get('id'))
 		price.append(house_detail_all.get('data')[house].get('price'))
 
 	# Check file exist or not
+	# 爬完的資料儲存進檔案
+	# 目前採用絕對路徑
+	## Best Solution: 改成相對路徑
 	filepath =r"C:\\Users\\user\\Desktop\\python\\591_scrapy\\postID_%s.txt" % currenttime
 	if os.path.isfile(filepath):
 		print("The file postID_%s.txt is here" % currenttime)
@@ -66,13 +76,16 @@ def main():
 	else:
 		print("The file postID_%s.txt isn't here, plz create one" % currenttime)
 		filecreate()
+	
+	# Find new_item and delete_item
+	#differnt function 會 return tuple
 	different_info=different()
+	# new item count
 	newitem_size = len(different_info[0])
+	# delete item count
 	deleteitem_size = len(different_info[2])
-	# Generate house detail url
+	
 	print('flag: %d' % flag)
-	#detail_price_message = "This House Price is %s" % detail_price # Get House Price
-	#print(detail_price_message)
 	# Call Line Notify API
 	if flag == 0:
 		print("Data isn't update")
@@ -88,51 +101,51 @@ def main():
 			linenotification.notify("Delete House : \n Price is: %s\n URL:\n %s "% (detail_price2 ,detail_url2))
 	else:
 		pass
+
+# Create a file to store postID
+def filecreate():
+	f = open("postID_%s.txt" % currenttime , "w+")
+	for postwrite in range(len(postID)):
+		f.write("%s price is %s\r\n" % (postID[postwrite],price[postwrite]))
+	f.close()
+
+# repeat file exist, create and rename new file 
+def duplicatefilecreate():
+	f = open("postID_%s_new.txt" % currenttime , "w+")
+	for duplicatedata in range(len(postID)):
+		f.write("%s price is %s\r\n" % (postID[duplicatedata],price[duplicatedata]))
+	f.close()
+
+
 # Read Different Data
 def different():
+	# 判斷是否又新的房子等待出租
+	# 首先開啟紀錄新房子的檔案
 	with open("New_item.txt" , "r") as f1 :
 		content = f1.readlines()
 		for newhouse in range(len(content)):
 			detail_line = content[newhouse]
-			detail_post_ID = re.split(' price is ', detail_line)[0]
-			detail_price = re.split(' price is ', detail_line)[1]
-			#print(detail_line)
-			new_postID.append(detail_post_ID)
-			new_price.append(detail_price)
-			#print(type(detail_line))
-			#new_info = re.split(' price is ', detail_new[1])
+			detail_post_ID = re.split(' price is ', detail_line)[0] # 取得新房子ID
+			detail_price = re.split(' price is ', detail_line)[1]	# 取得新房子price	
+			new_postID.append(detail_post_ID) # 將新房子的ID 寫進 new_postID 陣列
+			new_price.append(detail_price) # 將新房子的價錢 寫進 new_price 陣列
 	f1.close()
 	
 	with open("Delete_item.txt", "r") as f_readdel:
 		content = f_readdel.readlines()
 		for deletehouse in range(len(content)):
 			detail_line = content[deletehouse]
-			detail_post_ID = re.split(' price is ',detail_line)[0]
-			detail_price = re.split(' price is ', detail_line)[1]
-			delete_postID.append(detail_post_ID)
-			delete_price.append(detail_price)
+			detail_post_ID = re.split(' price is ',detail_line)[0] # 取得被刪除的房子ID
+			detail_price = re.split(' price is ', detail_line)[1]  # 取得被刪除的房子price
+			delete_postID.append(detail_post_ID) # 將被刪除的房子的ID 寫進delete_postID 陣列
+			delete_price.append(detail_price)    # 將被刪除的房子的ID 寫進delete_price 陣列
 	f_readdel.close()
-
+	# return 四個陣列(tuple)
 	return new_postID, new_price, delete_postID, delete_price
 	
-
-# Create a file to store postID
-def filecreate():
-	f = open("postID_%s.txt" % currenttime , "w+")
-	for postwrite in range(len(postID)):
-		#f.write("Post ID is %s\r\n" % postID[j])
-		f.write("%s price is %s\r\n" % (postID[postwrite],price[postwrite]))
-	f.close()
-def duplicatefilecreate():
-	f = open("postID_%s_new.txt" % currenttime , "w+")
-	for duplicatedata in range(len(postID)):
-		#f.write("Post ID is %s\r\n" % postID[k])
-		f.write("%s price is %s\r\n" % (postID[duplicatedata],price[duplicatedata]))
-	f.close()
-
+# 比較新舊檔案的差別，並找出新增的房子
 def filecompare_newitem():
 	global flag
-	#print("filecompare_new: {}".format(flag))
 	# List new item and write to file 
 	with open("postID_%s.txt" % currenttime, "r") as file_old:
 		old = file_old.readlines()
@@ -147,7 +160,7 @@ def filecompare_newitem():
 		write_new.close()
 		flag = 1
 	return flag
-
+# 比較新舊檔案的差別，並找出刪除的房子
 def filecompare_deleteitem():
 	global flag
 	#print("filecompare_delete: {}".format(flag))	
@@ -171,8 +184,6 @@ def filecompare_deleteitem():
 	
 
 if __name__ == '__main__':
-	#global flag
-	#flag = flag + 0
 	#while(1):
 	main()
 	# Delete old file and rename new file
